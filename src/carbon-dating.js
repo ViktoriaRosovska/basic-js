16944ed57c2392176bfec6771c796432c4f6ff8b
Viktoria Rosovska <torimeister@gmail.com>
2024-12-30 22:00:17 +0200

    feat: solve a task carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample( sampleActivity) {
+  if (parseFloat(sampleActivity) <= 0 || parseFloat(sampleActivity) >= 15 || Number.isNaN(parseFloat(sampleActivity)) || parseFloat(sampleActivity) === Infinity || parseFloat(sampleActivity) === -Infinity || typeof(sampleActivity) !== "string") return false;
+  else {  
+    return Math.ceil(Math.log2(MODERN_ACTIVITY / parseFloat(sampleActivity)) * HALF_LIFE_PERIOD / Math.log2(2));
+  }
 }
 
 module.exports = {
